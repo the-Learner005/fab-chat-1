2df62a97a6c01d52406caff29bb6f5262b1e8360
musmanpak <musman.pak0@gmail.com>
2018-05-06 21:02:21 +0500

    Remove Error 2

--- server/server.js
+++ server/server.js
@@ -1,39 +1,50 @@
+const path = require('path');
 const http = require('http');
 const express = require('express');
-const path = require('path');
-const socketIO= require('socket.io');
+const socketIO = require('socket.io');
 
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
 var app = express();
 var server = http.createServer(app);
-var io= socketIO(server);
-var publicPath = path.join(__dirname , '../public');
-var PORT = process.env.PORT || 3000;
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-io.on('connection',function (socket) {
-    console.log("User Connected");
-    //____________________//
-    socket.on('disconnect',function(){
-        console.log("User Disconnected");
-    });
-    // socket.emit('newEmail',{
-    //     from:'Ayesha',
-    //     to:'Asma',
-    //     createdAt: new Date().toString()
-    //
-    // });
+io.on('connection', function(socket){
+    console.log('New user connected');
 
-    socket.on('createMessage',function(message){
-        //is msg ko sbko send krdena hai
-        io.emit('newMessage',{
-            from: message.from,
-            text: message.text,
-            date: new Date().getTime()
-        });
-    });
+socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
 });
 
-server.listen(PORT,function(){
-    console.log("Server is ready on port " +  PORT);
+socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+});
+
+socket.on('createMessage', function(message) {
+    console.log('createMessage', message);
+io.emit('newMessage', {
+    from: message.from,
+    text: message.text,
+    createdAt: new Date().getTime()
+});
+// socket.broadcast.emit('newMessage', {
+//   from: message.from,
+//   text: message.text,
+//   createdAt: new Date().getTime()
+// });
+});
+
+socket.on('disconnect', function() {
+    console.log('User was disconnected');
+});
+});
+
+server.listen(port, function(){
+    console.log("Server is up on" +port);
 });
